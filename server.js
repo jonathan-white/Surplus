34015fc746e4b7060f1b14ffd1f40ad56c1c5788
Jon White <jon.white2@gmail.com>
2018-06-23 20:09:12 -0400

    port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 443;
 // require("dotenv").config();
 
 app.use(logger("dev"));
