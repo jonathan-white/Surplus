935ef3cb0576fbd8437cdd596d3b340488bdb206
Mona <kbeeler07@gmail.com>
2018-06-25 20:59:59 -0400

    new Product

--- client/src/components/NewProduct/NewProduct.js
+++ client/src/components/NewProduct/NewProduct.js
@@ -1,15 +1,106 @@
-import React from "react";
-import { Card, CardTitle } from 'react-materialize'
-import "./NewProduct.css";
 
-const NewProduct = props => (
-	<Card header={<CardTitle reveal image={"images/officeChairO.jpg"} waves='light' />}
-		// this is accessing through props the market.js state for cards
-		// title= {props.nonsense.title}
-		title="Card Title"
-		reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-		<p><a href={`/products/${props.id}`}>This is a link</a></p>
-	</Card>
-)
+export default NewProduct;import React, { Component } from "react";
+// import { Card, CardTitle } from 'react-materialize'
+import "./NewProduct.css";
+import Input from "react-materialize/lib/Input";
+import API from "../../utils/API";
+
+class NewProduct extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			title: "",
+			description: "",
+			price: "",
+			quantity: ""
+
+		}
+	};
+
+	handleFormSubmit = event => {
+		event.preventDefault();
+
+		const newProduct = {
+			title: this.state.title,
+			description: this.state.description,
+			price: this.state.price,
+			quantity: this.state.quantity
+
+		}
+
+		API.createProduct(newProduct)
+			.then(res => console.log(res.data))
+			.catch(err => console.log(err));
+	};
+
+	handleInputChange = event => {
+		const {name, value} = event.target;
+
+		this.setState({
+			[name]: value
+		});
+	};
+
+	render() {
+		return (
+
+			<div className="profile">
+				<div className="profile-img">
+					<img src="images/chase.jpg" alt="company logo" />
+					<a className="btn-floating btn-large waves-effect waves-light red"><i className="material-icons">add</i></a>
+				</div>
+				<form className="row">
+					<Input 
+						s={12} 
+						type="text"
+						label="Title (required)" 
+						value={this.state.title}
+						name="title"
+						onChange={this.handleInputChange}
+					/>
+					<Input 
+						s={12} 
+						type="textarea" 
+						label="description" 
+						value={this.state.description}
+						name="description"
+						onChange={this.handleInputChange}
+					/>
+					<Input 
+						s={12} 
+						type="text" 
+						label="price" 
+						value={this.state.price}
+						name="price"
+						onChange={this.handleInputChange}
+					/>
+
+					<Input 
+						s={12} 
+						type="number" 
+						label="quantity" 
+						value={this.state.quantity}
+						name="quantity"
+						onChange={this.handleInputChange}
+					/>
+					<Input 
+						s={12} 
+						type="tel" 
+						label="Phone" 
+						value={this.state.phone}
+						name="phone"
+						onChange={this.handleInputChange}
+					/>
+					<button 
+						className="btn center-align" 
+						onClick={this.handleFormSubmit}
+					>
+					Submit
+					</button>
+				</form>
+			</div>
+		)
+	}
+} 
 
 export default NewProduct;
