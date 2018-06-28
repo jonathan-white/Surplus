dfba780ea610c2e6e8ba5b9df95e7d318a63abc2
Jon White <jon.white2@gmail.com>
2018-06-28 05:24:07 -0400

    created route for static images

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
+// Allow images stored in the images folder to be served up
+router.get("/images/:filename", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build/images/" + req.params.filename));
+});
+
 // If no API routes are hit, send the React app
 router.use("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../build/index.html"));
