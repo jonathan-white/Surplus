95b8cc77182e595c51bfc9b31bd0a928e63345c9
Jon White <jon.white2@gmail.com>
2018-06-28 05:08:56 -0400

    added uploads folder to gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ const App = () => (
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/checkout" component={ShoppingCart} />
         <Route exact path="/login" component={Login} />
-        {/* <Route component={NoMatch} /> */}
+        <Route component={NoMatch} />
       </Switch>
       <Footer />
     </div>
