a894c975c225be8b15769cbf1ada00865e80500e
mkondray <mkondray@gmail.com>
2018-06-30 13:27:29 -0400

    Added Results.js Page

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -32,30 +32,4 @@ class Navigation extends Component {
 }; 
 
 
-// const Navigation = props => (
-//     <Navbar brand='Surplus Market' right>
-//       <NavItem href='/search'>
-//         <Icon>search</Icon>
-//       </NavItem>
-//       {props.isLoggedIn && (
-//         <NavItem href='/profile'>
-//           <Icon>person</Icon>
-//         </NavItem>
-//       )}
-//       <NavItem href='/checkout'>
-//         <Icon>shopping_cart</Icon>
-//       </NavItem>
-//       {props.isLoggedIn ? (
-//         <NavItem href='/signout'>
-//           Sign out
-//         </NavItem>
-//       ) : (
-//         <NavItem href='/signin'>
-//           Sign in
-//         </NavItem>
-//       )}
-//     </Navbar>
-// );
-
-
 export default Navigation;
