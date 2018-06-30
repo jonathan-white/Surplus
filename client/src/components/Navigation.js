08e4d3e77bd9dc6bfcccef7031429833d72fc276
mkondray <mkondray@gmail.com>
2018-06-30 14:08:09 -0400

    Update to Search Bar Functionality

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,15 +5,31 @@ import { Navbar, NavItem, Icon } from 'react-materialize'
 class Navigation extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {searchTerms: ''};
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }; 
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // this.context.history.push('/search') OR this.props.history.push('/search')
+    // pass this.state.searchTerms to the route
+    // the search route has a component that makes the db request and loops through
+    // the results and creates a SearchResult component for each.
+  }
+
+
   render() {
     return (
-        <Navbar brand='Surplus Market' right>
+        <Navbar brand='Surplus Market' right style={{height: '75px'}}>
           <NavItem href="#">
-            <form>
-              <input type="text" placeholder="search for products"/>
+            <form onSubmit={this.handleSubmit}>
+              <input value={this.state.searchTerms} type="text" placeholder="search for products"/>
             </form>
           </NavItem>
           <NavItem href='/profile'>
@@ -30,6 +46,32 @@ class Navigation extends Component {
       );
   };
 }; 
+
+
+// const Navigation = props => (
+//     <Navbar brand='Surplus Market' right>
+//       <NavItem href='/search'>
+//         <Icon>search</Icon>
+//       </NavItem>
+//       {props.isLoggedIn && (
+//         <NavItem href='/profile'>
+//           <Icon>person</Icon>
+//         </NavItem>
+//       )}
+//       <NavItem href='/checkout'>
+//         <Icon>shopping_cart</Icon>
+//       </NavItem>
+//       {props.isLoggedIn ? (
+//         <NavItem href='/signout'>
+//           Sign out
+//         </NavItem>
+//       ) : (
+//         <NavItem href='/signin'>
+//           Sign in
+//         </NavItem>
+//       )}
+//     </Navbar>
+// );
 
 
 export default Navigation;
