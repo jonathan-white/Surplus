7d9bd037639f0815259754d505ad722459840029
Jon White <jon.white2@gmail.com>
2018-07-01 00:28:27 -0400

    GCP

--- routes/api/uploads.js
+++ routes/api/uploads.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const multer = require("multer");
 const bodyParser = require('body-parser');
 const path = require("path");
+// const Storage = require('@google-cloud/storage');
 
 router.use(bodyParser.json());
 
-const Storage = multer.diskStorage({
+const mStorage = multer.diskStorage({
 	destination: function(req, file, callback) {
 		callback(null, "./uploads");
 	},
@@ -15,9 +16,9 @@ const Storage = multer.diskStorage({
 });
 
 const upload = multer({
-    storage: Storage
+    storage: mStorage
   }).single('fileUpload');
-  
+
 // Matches with "/api/uploads"
 router.route("/")
   .post(function(req, res) {
@@ -27,7 +28,14 @@ router.route("/")
             return res.end("Something went wrong!");
         }
         req.file.url = `/api/uploads/${req.file.filename}`;
-        console.log(req.file);
+        console.log(req.file); //TODO: Remove
+
+				const bucketName = process.env.BUCKET;
+				const filename = path.join(__dirname, "../../uploads/" + req.file.filename);
+
+				console.log('filename:',filename);
+				uploadFile(bucketName, filename);
+
         return res.json(req.file);
         // return res.end("File uploaded sucessfully!.");
     });
@@ -37,5 +45,30 @@ router.route("/")
 router.get("/:filename", (req, res) => {
     res.sendFile(path.join(__dirname, "../../uploads/" + req.params.filename));
 });
+
+// =========================
+// Google Cloud Integration
+// =========================
+const uploadFile = (bucketName, filename) => {
+	// Imports the Google Cloud client library
+	const Storage = require('@google-cloud/storage');
+
+	// Creates a client
+	const projectId = process.env.PROJECT_ID;
+
+	const storage = new Storage({ projectId });
+
+	// Uploads a local file to the bucket
+	storage
+	.bucket(bucketName)
+	.upload(filename)
+	.then(() => {
+		console.log(`${filename} uploaded to ${bucketName}.`);
+	})
+	.catch(err => {
+		console.error('ERROR:', err);
+	});
+
+}
 
 module.exports = router;
