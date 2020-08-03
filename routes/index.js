4155070216ca8b159ce12aed4da7871d4874ee8d
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2020-08-03 02:32:59 -0400

    package updates

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const productsController = require("../controllers/productsController");
 
 // API Routes
 router.use("/api", apiRoutes);
--- server.js
+++ server.js
@@ -30,11 +30,12 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-// If deployed, use the deployed database. Otherwise use the local nytreact database
+// If deployed, use the deployed database. Otherwise use the local database
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/surplusDB");
+mongoose.connect(process.env.DB_URI || "mongodb://localhost:27017/surplusDB", 
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Connect to MongoDB &
+// Connect to MongoDB
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
